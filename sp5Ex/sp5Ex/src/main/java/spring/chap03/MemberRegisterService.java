9424d83ccaff94ca85e9e4c58d8ef9206a687c5b
unknown <nywogud@hanmail.net>
2020-12-17 22:07:33 +0900

    springEx

--- sp5Ex/sp5Ex/src/main/java/spring/chap03/MemberRegisterService.java
+++ sp5Ex/sp5Ex/src/main/java/spring/chap03/MemberRegisterService.java
@@ -1,9 +1,14 @@
 package spring.chap03;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import java.time.LocalDateTime;
 
 public class MemberRegisterService {
+    @Autowired
     private  MemberDao memberDao;
+
+    public MemberRegisterService(){}
 
     public MemberRegisterService(MemberDao memberDao){
         this.memberDao = memberDao;
